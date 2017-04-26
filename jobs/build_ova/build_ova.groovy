6a44e1fdadca5becfc0fc9762a23b26f7dbf3637
changev <alan.wei@emc.com>
2017-04-26 15:01:37 +0800

    f

--- jobs/build_ova/build_ova.groovy
+++ jobs/build_ova/build_ova.groovy
@@ -27,8 +27,8 @@ node(build_ova_node){ws{
 
                 timeout(180){
                     withEnv(["WORKSPACE=${current_workspace}"]){
-                        sh 'mkdir -p build/docker'
-                        sh 'wget http://10.62.59.175:8080/job/MasterCI/98/artifact/build/packer/rackhd-ubuntu-14.04-2.1.0-20170413UTC.ova -O build/docker/rackhd-ubuntu-14.04-2.1.0-20170413UTC.ova'
+                        sh 'mkdir -p build/packer'
+                        sh 'wget http://10.62.59.175:8080/job/MasterCI/98/artifact/build/packer/rackhd-ubuntu-14.04-2.1.0-20170413UTC.ova -O build/packer/rackhd-ubuntu-14.04-2.1.0-20170413UTC.ova'
                     }
                 }
                 archiveArtifacts 'build/packer/*.ova, build/packer/*.log, build/packer/*.md5, build/packer/*.sha'
