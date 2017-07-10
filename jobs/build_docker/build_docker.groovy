948becf3b9e8882a2111e1b04c5e934be085aa46
changev <alan.wei@emc.com>
2017-07-10 11:14:04 +0800

    bugfix: lock docker slave

--- jobs/build_docker/build_docker.groovy
+++ jobs/build_docker/build_docker.groovy
@@ -1,38 +1,53 @@
-node(build_docker_node){
-    withEnv([
-        "MANIFEST_FILE_URL=${env.MANIFEST_FILE_URL}",
-        "IS_OFFICIAL_RELEASE=${env.IS_OFFICIAL_RELEASE}",
-        "BINTRAY_SUBJECT=${env.BINTRAY_SUBJECT}",
-        "BINTRAY_REPO=debian",
-        "CI_BINTRAY_SUBJECT=${env.CI_BINTRAY_SUBJECT}",
-        "CI_BINTRAY_REPO=debian",
-        "BINTRAY_COMPONENT=main",
-        "BINTRAY_DISTRIBUTION=trusty",
-        "BINTRAY_ARCHITECTURE=amd64",
-        "CLONE_DIR=b"]){
-        deleteDir()
-        def current_workspace = pwd()
-        dir("build-config"){
-            checkout scm
-        }
-        withCredentials([
-            usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
-                             passwordVariable: 'SUDO_PASSWORD',
-                             usernameVariable: 'SUDO_USER'),
-            usernameColonPassword(credentialsId: 'a94afe79-82f5-495a-877c-183567c51e0b', 
-                                  variable: 'BINTRAY_CREDS')]){
-            timeout(90){
-                withEnv(["WORKSPACE=${current_workspace}"]){
-                    sh './build-config/jobs/build_docker/build_docker.sh'
-                }
+import groovy.transform.Field;
+@Field def shareMethod
+node{
+    deleteDir()
+    checkout scm
+    shareMethod = load("jobs/ShareMethod.groovy")
+}
+
+String label_name = build_docker_label
+lock(label:label_name,quantity:1){
+    resources_name = shareMethod.getLockedResourceName(label_name)
+    if(resources_name.size>0){
+        node_name = resources_name[0]
+    }
+    node(node_name){
+        withEnv([
+            "MANIFEST_FILE_URL=${env.MANIFEST_FILE_URL}",
+            "IS_OFFICIAL_RELEASE=${env.IS_OFFICIAL_RELEASE}",
+            "BINTRAY_SUBJECT=${env.BINTRAY_SUBJECT}",
+            "BINTRAY_REPO=debian",
+            "CI_BINTRAY_SUBJECT=${env.CI_BINTRAY_SUBJECT}",
+            "CI_BINTRAY_REPO=debian",
+            "BINTRAY_COMPONENT=main",
+            "BINTRAY_DISTRIBUTION=trusty",
+            "BINTRAY_ARCHITECTURE=amd64",
+            "CLONE_DIR=b"]){
+            deleteDir()
+            def current_workspace = pwd()
+            dir("build-config"){
+                checkout scm
             }
-            archiveArtifacts 'rackhd_docker_images.tar, build_record'
-            stash name: 'docker', includes: 'rackhd_docker_images.tar, build_record'
+            withCredentials([
+                usernamePassword(credentialsId: 'ff7ab8d2-e678-41ef-a46b-dd0e780030e1',
+                                passwordVariable: 'SUDO_PASSWORD',
+                                usernameVariable: 'SUDO_USER'),
+                usernameColonPassword(credentialsId: 'a94afe79-82f5-495a-877c-183567c51e0b', 
+                                    variable: 'BINTRAY_CREDS')]){
+                timeout(90){
+                    withEnv(["WORKSPACE=${current_workspace}"]){
+                        sh './build-config/jobs/build_docker/build_docker.sh'
+                    }
+                }
+                archiveArtifacts 'rackhd_docker_images.tar, build_record'
+                stash name: 'docker', includes: 'rackhd_docker_images.tar, build_record'
+                env.DOCKER_WORKSPACE="${current_workspace}"
+                env.DOCKER_STASH_NAME="docker"
+                env.DOCKER_STASH_PATH="rackhd_docker_images.tar"
+                env.DOCKER_RECORD_STASH_PATH="build_record"
+            }
             env.DOCKER_WORKSPACE="${current_workspace}"
-            env.DOCKER_STASH_NAME="docker"
-            env.DOCKER_STASH_PATH="rackhd_docker_images.tar"
-            env.DOCKER_RECORD_STASH_PATH="build_record"
         }
-        env.DOCKER_WORKSPACE="${current_workspace}"
     }
 }
--- jobs/release/release_docker.groovy
+++ jobs/release/release_docker.groovy
@@ -1,17 +1,31 @@
-node(build_docker_node){
-    dir("on-build-config"){
-        checkout scm
+import groovy.transform.Field;
+@Field def shareMethod
+node{
+    deleteDir()
+    checkout scm
+    shareMethod = load("jobs/ShareMethod.groovy")
+}
+
+String label_name = build_docker_label
+lock(label:label_name,quantity:1){
+    resources_name = shareMethod.getLockedResourceName(label_name)
+    if(resources_name.size>0){
+        node_name = resources_name[0]
     }
-    dir("DOCKER"){
-        unstash env.DOCKER_STASH_NAME
-    }
-    withCredentials([
-        usernamePassword(credentialsId: 'da1e60c6-f23a-429d-b0f5-19e3b287f5dc', 
-                         passwordVariable: 'DOCKERHUB_PASS', 
-                         usernameVariable: 'DOCKERHUB_USER')]) {
-        timeout(120){
-            sh './on-build-config/jobs/release/release_docker.sh'
+    node(node_name){
+        dir("on-build-config"){
+            checkout scm
+        }
+        dir("DOCKER"){
+            unstash env.DOCKER_STASH_NAME
+        }
+        withCredentials([
+            usernamePassword(credentialsId: 'da1e60c6-f23a-429d-b0f5-19e3b287f5dc', 
+                            passwordVariable: 'DOCKERHUB_PASS', 
+                            usernameVariable: 'DOCKERHUB_USER')]) {
+            timeout(120){
+                sh './on-build-config/jobs/release/release_docker.sh'
+            }
         }
     }
 }
-
