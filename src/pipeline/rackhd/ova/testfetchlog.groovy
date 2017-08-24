505c67e85f4841bcdf2dd3dabbc453ea0f8960e7
changev <alan.wei@emc.com>
2017-08-24 17:27:37 +0800

    f

--- src/pipeline/rackhd/ova/testfetchlog.groovy
+++ src/pipeline/rackhd/ova/testfetchlog.groovy
@@ -10,8 +10,7 @@ node{
                                                                 ova_dict['ova_url']='http://10.62.59.175:8080/job/alan/job/ova-source/lastSuccessfulBuild/artifact/rackhd-ubuntu-14.04-2.18.0-20170822UTC.ova'
                                                                         def ova_resource = []
                                                                                 println "start to run test"
-                                                                                        functionTest.runTest("virtual_stack","FIT",ova_resource, ova_dict, true, 30, "true")
+                                                                                        functionTest.runTest("virtual_stack","FIT",ova_resource, ova_dict, false, 30, "true")
 
 
 }
-
